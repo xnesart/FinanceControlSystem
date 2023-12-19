20ba10cad1d7189c77f5a645a9d484087033d566
xnesart <xnesart@mail.ru>
2023-12-19 17:44:36 +0300

    sync

--- FinanceControlSystem.UI/MainWindow.xaml.cs
+++ FinanceControlSystem.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using FinanceControlSystem.Logics;
+using FinanceControlSystem.Logics.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,14 +18,57 @@ namespace FinanceControlSystem.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AccountClient _accountClient;
+
         public MainWindow()
         {
             InitializeComponent();
+            _accountClient = new AccountClient();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
 
+        private void ButtonAddAccount_Click(object sender, RoutedEventArgs e)
+        {
+            AccountModel accountModel = new AccountModel()
+            {
+                Name = TextBoxName.Text,
+                Type = TextBoxType.Text,
+                Balance = decimal.Parse(TextBoxBalance.Text),
+            };
+            if (accountModel.Name == "" || accountModel.Type == "" )
+            {
+                return;
+            }
+            _accountClient.AddAccount(accountModel);
+            string name = TextBoxName.Text;
+            string type = TextBoxType.Text;
+            string balance = TextBoxBalance.Text;
+            ListBoxListOfAccounts.Items.Add($"{name} и {type} и {balance}");
+            TextBoxName.Text = "";
+            TextBoxType.Text = "";
+            TextBoxBalance.Text = "";
+        }
+
+        private void TextBoxBalance_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Предотвращаем ввод пробелов
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBoxBalance_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Предотвращаем ввод символа, если это не цифра или точка
+            if (!char.IsDigit(e.Text, 0) && e.Text != ".")
+            {
+                e.Handled = true; 
+            }
+            if(e.Text == " ")
+            {
+                e.Handled = true;
+            }
         }
     }
 }
