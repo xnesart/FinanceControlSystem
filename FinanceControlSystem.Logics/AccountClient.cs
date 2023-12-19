3ba8a24741b50ec50c982b1cc818ddfa8de7e3d8
xnesart <xnesart@mail.ru>
2023-12-19 17:01:44 +0300

    init

---
+++ FinanceControlSystem.Logics/AccountClient.cs
@@ -0,0 +1,18 @@
+﻿using FinanceControlSystem.Logics.Models;
+
+namespace FinanceControlSystem.Logics
+{
+    public class AccountClient
+    {
+        private List<AccountModel> _accounts;
+        public AccountClient()
+        {
+            _accounts = new List<AccountModel>();
+        }
+
+        public void AddAccount(AccountModel account)
+        {
+            _accounts.Add(account);
+        }
+    }
+}
---
+++ FinanceControlSystem.Logics/Models/AccountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceControlSystem.Logics.Models
+{
+    public class AccountModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
